9cdd69944d10516ddd83ac399ba1fb6bc4e98f4c
pavan1929 <pavan.koilada@gmail.com>
2021-06-15 16:34:08 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# url-shortner-angular
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/url-shortner-angular)
---
+++ src/app/intro.component.ts
@@ -0,0 +1,9 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'intro',
+  template: `<h1>Tired of sharing long and clumsy URL? Shorten it to make it readible & shareble!</h1>`,
+  styles: [`h1 { font-family: Lato; }`]
+})
+export class IntroComponent  {
+}
